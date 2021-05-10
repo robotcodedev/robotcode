609ec26f6d264b6d040dc4c2213af9c30407dd47
Daniel Biehl <daniel.biehl@imbus.de>
2021-05-10 10:23:01 +0200

    rename src folder to vscode-client, implement 4SpacesTab

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const path = require('path');
 const config = {
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
 
-  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  entry: './vscode-client/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
     path: path.resolve(__dirname, 'out'),
