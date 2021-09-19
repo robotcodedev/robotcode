dda543e0bea029b35891e56dbbae783d7927ab04
Daniel Biehl <daniel.biehl@imbus.de>
2021-09-19 23:23:29 +0200

    implement auto updating changelog if a new version is created

--- scripts/updateVersions.js
+++ scripts/updateVersions.js
@@ -1,18 +1,25 @@
+const { EOL } = require("os");
 const { env } = require("process");
 const { replaceInFile } = require("replace-in-file");
 
-const config = {
+const versionFilesOption = {
   files: ["robotcode/_version.py", "pyproject.toml"],
   from: /(^_*version_*\s*=\s*['"])([^'"]*)(['"])/gm,
   to: "$1" + env.npm_package_version + "$3",
 };
-replaceInFile(config, function (error, results) {
+replaceInFile(versionFilesOption, function (error, _results) {
   if (error) {
     console.error(error);
   }
-  if (results) {
-    for (const result of results) {
-      console.log(`${result.file} has ${result.hasChanged ? "" : "not "}changed`);
-    }
+});
+
+const changelogOptions = {
+  files: ["CHANGELOG.md"],
+  from: /^(\#*\s*)(\[Unreleased\])$/gm,
+  to: "$1$2" + EOL + EOL + "$1 " + env.npm_package_version,
+};
+replaceInFile(changelogOptions, function (error, _results) {
+  if (error) {
+    console.error(error);
   }
 });
