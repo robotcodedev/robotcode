69f702beb9c4654496f8acbff0925b6b771abcbb
Daniel Biehl <daniel.biehl@imbus.de>
2021-10-22 21:51:19 +0200

    make paths in test platform independed

--- tests/robotcode/language_server/robotframework/tools.py
+++ tests/robotcode/language_server/robotframework/tools.py
@@ -13,7 +13,7 @@ def generate_tests_from_source_document(
     path: str,
 ) -> Generator[Union[Tuple[str, str, int, int, str], Any], None, None]:
     file = Path(path).relative_to(Path(".").parent.absolute())
-
+    file_str = str(file).replace("\\", "/")
     current_line = 0
     for line, text in enumerate(file.read_text().splitlines()):
 
@@ -26,16 +26,16 @@ def generate_tests_from_source_document(
             if name and expression:
                 if skip:
                     yield pytest.param(
-                        str(file), name, current_line, start, expression, marks=pytest.mark.skip(reason="TODO")
+                        file_str, name, current_line, start, expression, marks=pytest.mark.skip(reason="TODO")
                     )
                 else:
                     if end - start == 1:
-                        yield str(file), name, current_line, start, expression
+                        yield file_str, name, current_line, start, expression
                     else:
-                        yield str(file), name, current_line, start, expression
+                        yield file_str, name, current_line, start, expression
                         if end - start > 2:
-                            yield str(file), name, current_line, int(start + (end - start) / 2), expression
+                            yield file_str, name, current_line, int(start + (end - start) / 2), expression
 
-                        yield str(file), name, current_line, end - 1, expression
+                        yield file_str, name, current_line, end - 1, expression
         else:
             current_line = line
