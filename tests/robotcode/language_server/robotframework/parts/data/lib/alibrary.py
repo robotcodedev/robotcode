b8e09d666a0489190ac8fbc67dd810d4162f2d20
Daniel Biehl <daniel.biehl@imbus.de>
2022-02-09 13:10:37 +0100

    add missing files to test data

---
+++ tests/robotcode/language_server/robotframework/parts/data/lib/alibrary.py
@@ -0,0 +1,11 @@
+from typing import Any
+from robot.api import logger
+
+
+class alibrary:  # noqa: N801
+    def __init__(self, a_param: Any = None) -> None:
+        self.a_param = a_param
+
+    def a_library_keyword(self) -> Any:
+        logger.info("hello from a_library")
+        return self.a_param
