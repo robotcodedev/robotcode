4b1bf5aa44f5a0f67821b96ed63d5de6c2a9ed27
Daniel Biehl <daniel.biehl@imbus.de>
2022-03-08 00:28:45 +0100

    rewrote update_versions script in python

---
+++ scripts/update_versions.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+import re
+from semantic_version import Version
+
+
+def replace_in_file(filename: Path, pattern: "re.Pattern[str]", to: str) -> None:
+    text = filename.read_text()
+    new = pattern.sub(to, text)
+    filename.write_text(new)
+
+
+def main() -> None:
+    # version = Version(os.environ["npm_package_version"])
+    version = Version("0.7.1")
+    preview = version.minor % 2 != 0
+
+    for f in ["robotcode/_version.py", "pyproject.toml"]:
+        replace_in_file(
+            Path(f),
+            re.compile(r"""(^_*version_*\s*=\s*['"])([^'"]*)(['"])""", re.MULTILINE),
+            rf"\g<1>{version or ''}{'-preview' if preview else ''}\g<3>",
+        )
+
+    for f in ["CHANGELOG.md"]:
+        replace_in_file(
+            Path(f),
+            re.compile(r"^(\#*\s*)(\[Unreleased\])$", re.MULTILINE),
+            rf"\g<1>\g<2>{os.linesep}- none so far{os.linesep}\g<1> {version or ''}",
+        )
+
+
+if __name__ == "__main__":
+    main()
