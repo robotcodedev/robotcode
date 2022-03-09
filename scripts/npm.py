ff6fe9bb278c839bed51e5d3e916896ed115e1c2
Daniel Biehl <daniel.biehl@imbus.de>
2022-03-09 15:08:08 +0100

    correct poetry npm script

--- scripts/npm.py
+++ scripts/npm.py
@@ -2,8 +2,9 @@ import sys
 from subprocess import run
 
 
-def main() -> None:
-    run(f"npm run {' '.join(sys.argv[1:])}", shell=True).check_returncode()
+def main() -> int:
+    args = f"npm {' '.join(sys.argv[1:])}"
+    return run(args, shell=True).returncode
 
 
 if __name__ == "__main__":
