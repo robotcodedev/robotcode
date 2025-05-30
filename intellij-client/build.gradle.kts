22a3449822ca5a203c195a57b7c51f46e3eaad6b
Daniel Biehl <dbiehl@live.de>
2025-05-30 15:17:42 +0200

    chore(intellij): add python plugin to runIdeIntellijIdeaC task

--- intellij-client/build.gradle.kts
+++ intellij-client/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
 // Configure project's dependencies
 repositories {
     mavenCentral()
-    
+
     // IntelliJ Platform Gradle Plugin Repositories Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-repositories-extension.html
     intellijPlatform {
         defaultRepositories()
@@ -42,10 +42,10 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
     compileOnly(libs.kotlinxSerialization)
-    
+
     testImplementation(kotlin("test"))
     testImplementation(libs.junit)
-    
+
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         create(
@@ -53,11 +53,11 @@ dependencies {
             providers.gradleProperty("platformVersion"),
             useInstaller = false,
         )
-        
+
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
-        
+
         pluginVerifier()
         zipSigner()
         testFramework(TestFrameworkType.Platform)
@@ -69,12 +69,12 @@ intellijPlatform {
     pluginConfiguration {
         name = providers.gradleProperty("pluginName")
         version = providers.gradleProperty("pluginVersion")
-        
+
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
             val start = "<!-- Plugin description -->"
             val end = "<!-- Plugin description end -->"
-            
+
             with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
@@ -82,9 +82,9 @@ intellijPlatform {
                 subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
             }
         }
-        
+
         val changelog = project.changelog
-        
+
         // local variable for configuration cache compatibility // Get the latest available change notes from the changelog file
         changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
             with(changelog) {
@@ -94,29 +94,29 @@ intellijPlatform {
                 )
             }
         }
-        
+
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
             untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
-    
+
     signing {
         certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
         privateKey = providers.environmentVariable("PRIVATE_KEY")
         password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
     }
-    
+
     publishing {
         token = providers.environmentVariable("PUBLISH_TOKEN")
-        
+
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
         channels = providers.gradleProperty("pluginVersion")
             .map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
-    
+
     pluginVerification {
         failureLevel = listOf(INVALID_PLUGIN, COMPATIBILITY_PROBLEMS, MISSING_DEPENDENCIES)
         verificationReportsFormats = listOf(MARKDOWN, PLAIN)
@@ -130,7 +130,7 @@ intellijPlatform {
                 )
             }
         }
-        
+
     }
 }
 
@@ -163,22 +163,22 @@ val prepareSandboxConfig: PrepareSandboxTask.() -> Unit = {
 
 tasks {
     runIde {
-        
+
         // From https://app.slack.com/client/T5P9YATH9/C5U8BM1MK
         // systemProperty("ide.experimental.ui", "true")
         // systemProperty("projectView.hide.dot.idea", "false")
         // systemProperty("terminal.new.ui", "false")
         // systemProperty("ide.tree.painter.compact.default", "true")
     }
-    
+
     wrapper {
         gradleVersion = providers.gradleProperty("gradleVersion").get()
     }
-    
+
     publishPlugin {
         dependsOn(patchChangelog)
     }
-    
+
     prepareSandbox(prepareSandboxConfig)
 }
 
@@ -204,9 +204,9 @@ tasks.withType<KotlinCompile> {
             )
         }
     }
-    
+
     prepareSandboxTask(prepareSandboxConfig)
-    
+
     plugins {
         robotServerPlugin(Constraints.LATEST_VERSION)
     }
@@ -214,19 +214,22 @@ tasks.withType<KotlinCompile> {
 
 @Suppress("unused") val runIdePyCharmProf by intellijPlatformTesting.runIde.registering {
     type = IntelliJPlatformType.PyCharmProfessional
-    
+
     prepareSandboxTask(prepareSandboxConfig)
 }
 
 @Suppress("unused") val runIdePyCharmCommunityEAP by intellijPlatformTesting.runIde.registering {
     type = IntelliJPlatformType.PyCharmCommunity
     version = "LATEST-EAP-SNAPSHOT"
-    
+
     prepareSandboxTask(prepareSandboxConfig)
 }
 
 @Suppress("unused") val runIdeIntellijIdeaC by intellijPlatformTesting.runIde.registering {
     type = IntelliJPlatformType.IntellijIdeaCommunity
-    
+    plugins {
+        plugin("PythonCore:251.23774.435")
+    }
+
     prepareSandboxTask(prepareSandboxConfig)
 }
