987ea421f22b133851a462b921b7c3e1e5802a4a
Daniel Biehl <dbiehl@live.de>
2025-06-12 08:24:21 +0200

    fix(listener): add 'robocop.toml' to monitored project files to allow detection of changes in robocop configuration

--- intellij-client/src/main/kotlin/dev/robotcode/robotcode4ij/listeners/RobotCodeVirtualFileListener.kt
+++ intellij-client/src/main/kotlin/dev/robotcode/robotcode4ij/listeners/RobotCodeVirtualFileListener.kt
@@ -7,9 +7,9 @@ import dev.robotcode.robotcode4ij.restartAll
 
 class RobotCodeVirtualFileListener(private val project: Project) : AsyncFileListener {
     companion object {
-        val PROJECT_FILES = arrayOf("robot.toml", ".robot.toml", "pyproject.toml")
+        val PROJECT_FILES = arrayOf("robot.toml", ".robot.toml", "pyproject.toml", "robocop.toml")
     }
-    
+
     override fun prepareChange(events: List<VFileEvent>): AsyncFileListener.ChangeApplier? {
         return object : AsyncFileListener.ChangeApplier {
             override fun afterVfsChange() {
--- vscode-client/extension/languageclientsmanger.ts
+++ vscode-client/extension/languageclientsmanger.ts
@@ -207,7 +207,7 @@ export class LanguageClientsManager {
     public readonly outputChannel: vscode.OutputChannel,
   ) {
     const fileWatcher1 = vscode.workspace.createFileSystemWatcher(
-      `**/{pyproject.toml,robot.toml,.robot.toml,robocop.toml,.gitignore,.robotignore}`,
+      `**/{pyproject.toml,robot.toml,.robot.toml,robocop.toml,.gitignore,.robotignore,robocop.toml}`,
     );
     fileWatcher1.onDidCreate((uri) => this.restart(vscode.workspace.getWorkspaceFolder(uri)?.uri));
     fileWatcher1.onDidDelete((uri) => this.restart(vscode.workspace.getWorkspaceFolder(uri)?.uri));
