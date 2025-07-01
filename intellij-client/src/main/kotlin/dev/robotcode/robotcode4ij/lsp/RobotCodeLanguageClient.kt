5031a36901c2abf98c7856f1c0c706ac8c997b34
Daniel Biehl <dbiehl@live.de>
2025-07-01 20:52:27 +0200

    chore(intellij): update to lsp4ij 0.14.2

--- intellij-client/src/main/kotlin/dev/robotcode/robotcode4ij/lsp/RobotCodeLanguageClient.kt
+++ intellij-client/src/main/kotlin/dev/robotcode/robotcode4ij/lsp/RobotCodeLanguageClient.kt
@@ -5,7 +5,7 @@ import com.redhat.devtools.lsp4ij.ServerStatus
 import com.redhat.devtools.lsp4ij.client.IndexAwareLanguageClient
 import dev.robotcode.robotcode4ij.configuration.RobotCodeProjectConfiguration
 
-class RobotCodeLanguageClient(project: Project?) : IndexAwareLanguageClient(project) {
+class RobotCodeLanguageClient(project: Project) : IndexAwareLanguageClient(project) {
     
     override fun handleServerStatusChanged(serverStatus: ServerStatus) {
         if (serverStatus == ServerStatus.started) {
