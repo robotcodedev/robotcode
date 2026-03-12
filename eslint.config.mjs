0f1ba6e0181b11ab612f1b0d56c14f484c8b51f3
Daniel Biehl <dbiehl@live.de>
2026-03-12 11:41:52 +0100

    chore: update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
@@ -24,7 +23,6 @@ export default [
   },
   { files: ["**/*.{ts,tsx}"] },
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
   {
