a8c39a377a74db9fe2a93e165415d0e805470d2d
Daniel Biehl <dbiehl@live.de>
2026-04-01 00:51:21 +0200

    docs(news): add supplementary release note for v2.5.0

--- docs/news/posts.data.ts
+++ docs/news/posts.data.ts
@@ -4,6 +4,7 @@ export default createContentLoader("news/*.md", {
   transform(raw) {
     return raw
       .filter((p) => p.url !== "/news/")
+      .filter((p) => !p.frontmatter.aprilFools || new Date() <= new Date("2026-04-02T12:00:00"))
       .sort((a, b) => {
         return +new Date(b.frontmatter.date) - +new Date(a.frontmatter.date);
       });
